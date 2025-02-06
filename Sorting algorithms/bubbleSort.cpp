f0beefe632c1c363a63db153f35f9207c6d8dea6
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-02-06 13:50:59 +0530

    Create bubbleSort.cpp

---
+++ Sorting algorithms/bubbleSort.cpp
@@ -0,0 +1,45 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+void printArr(int arr[], int arrSize)
+{   //cout << arr[0] << endl;
+    for (int j = 0; j < arrSize; j++)
+    {
+        cout << arr[j] << " ";
+    }
+    cout << endl;
+}
+
+
+
+int main() {
+    // Write C++ code here
+    const int arrSize = 5;
+    int arr[arrSize]  = {10,88,43,2,65};
+    
+    for (int i = 1; i < arrSize; i++)
+    {
+        int tmp;
+        for (int j = 0; j <  arrSize - 1; j++) {
+            if (arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                
+                printArr(arr, arrSize);
+                
+            }
+        }
+    
+            
+        printArr(arr, arrSize);
+        
+        
+    }
+        
+
+    return 0;
+}
