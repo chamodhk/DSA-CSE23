9ce283649f35f658dc31c6d2c75825437050cfb6
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-02-06 14:05:01 +0530

    Update bubbleSort.cpp
    added a flag to check if there is no swaps anymore so that we can stop the sorting.

--- Sorting algorithms/bubbleSort.cpp
+++ Sorting algorithms/bubbleSort.cpp
@@ -17,30 +17,38 @@ void printArr(int arr[], int arrSize)
 int main() {
     // Write C++ code here
     const int arrSize = 5;
-    int arr[arrSize]  = {10,88,43,2,65};
+    int arr[arrSize]  = {100,99,45,32,29};
     
     for (int i = 1; i < arrSize; i++)
     {
+        printArr(arr, arrSize);
         int tmp;
+        
+        bool noSwapFlag = true;
         for (int j = 0; j <  arrSize - i; j++) {
-            printArr(arr, arrSize);
+            
             if (arr[j] > arr[j+1])
             {
                 tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
+                noSwapFlag = false;
+                
                 
                 
                 
             }
         }
-    
+        
+        if (noSwapFlag == true) {
+            cout << "braked" << endl;
+            break;
             
-        
-        
+        }
+  
         
     }
-        
+    printArr(arr, arrSize);    
 
     return 0;
 }
